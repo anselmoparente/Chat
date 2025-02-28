e3df1281e1faae22150e4efc706bff5595644533
Anselmo Parente Martins <anselmoparente@gmail.com>
2025-02-28 00:54:26 -0300

    feat: Finish chat code

--- server.js
+++ server.js
@@ -9,14 +9,33 @@ const io = new Server(server, {
     cors: { origin: "*" }
 });
 
-io.on('connection', (socket) => {
-    console.log('connection');
+const generateRoomId = (user1, user2) => {
+    return [user1, user2].sort().join('_');
+};
 
-    socket.on('disconnect', (socket) => {
-        console.log('disconnect');
+const joinPrivateRoom = (socket, { user1, user2 }) => {
+    const room = generateRoomId(user1, user2);
+    socket.join(room);
+    console.log(`Usuário ${socket.id} entrou na sala privada: ${room}`);
+};
+
+const sendPrivateMessage = (socket, { user1, user2, text }) => {
+    const room = generateRoomId(user1, user2);
+    console.log(`Mensagem na sala privada ${room} de ${socket.id}: ${text}`);
+    io.to(room).emit('privateMessage', { id: socket.id, text });
+};
+
+io.on('connection', (socket) => {
+    console.log('Novo usuário conectado:', socket.id);
+
+    socket.on('joinPrivateRoom', (data) => joinPrivateRoom(socket, data));
+    socket.on('privateMessage', (data) => sendPrivateMessage(socket, data));
+
+    socket.on('disconnect', () => {
+        console.log('Usuário desconectado:', socket.id);
     });
 });
 
 server.listen(3000, () => {
-    console.log('Server is running');
+    console.log('Servidor rodando na porta 3000');
 });
